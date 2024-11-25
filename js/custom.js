359200d049b0d877dc8a3e3b7fb39c73334a1d69
Tulesh Ghelani <ghelanitulesh@gmail.com>
2024-11-25 08:43:10 +0530

    Created home page

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+
+  (function ($) {
+  
+  "use strict";
+
+  // NAVBAR
+  $('.navbar-nav .nav-link').click(function(){
+      $(".navbar-collapse").collapse('hide');
+  });
+
+  // PROJECTS IMAGE RESIZE
+    function NewsImageResize(){      
+      var LargeImage = $('.projects-thumb-small .projects-image').height();
+
+      $('.projects-thumb-large').css('height', LargeImage + 'px');
+    }
+
+    $(window).on("resize", NewsImageResize);
+    $(document).on("ready", NewsImageResize);
+
+    $('.custom-link').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 10;
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+  }
+});
+    
+  })(window.jQuery);
+
+// Initialize Projects Swiper
+const projectsSwiper = new Swiper('.projectsSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 1024px
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+    }
+});
+
+
